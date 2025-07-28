adc8ac8211facd35c525b7c898c2d1c4a366fcef
oscaralexander-weeks <oscarweeks@icloud.com>
2025-07-28 21:06:29 +0100

    Removed Cursor Hide

--- Assets/Scripts/UI/Scripts/RuntimeManager.cs
+++ Assets/Scripts/UI/Scripts/RuntimeManager.cs
@@ -23,7 +23,7 @@ public class RuntimeManager : MonoBehaviour
     private void Awake()
     {
         IsPaused = false;
-        Cursor.visible = false;
+        //Cursor.visible = false;
     }
 
     private IEnumerator EndStage()
@@ -36,14 +36,14 @@ public class RuntimeManager : MonoBehaviour
     {
         Time.timeScale = 0;
         IsPaused = true;
-        Cursor.visible = true;
+        //Cursor.visible = true;
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1;
         IsPaused = false;
-        Cursor.visible = false; 
+        //Cursor.visible = false; 
     }
 
     public void QuitGame()
